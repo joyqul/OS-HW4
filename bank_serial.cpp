a88c856be53c99a49ab63bf814d6096294688aac
Joyqul Chen <joy1357924680@gmail.com>
2014-01-22 11:35:37 +0800

    Use C++11 unordered map to speed up.

--- bank_serial.cpp
+++ bank_serial.cpp
@@ -1,17 +1,18 @@
 #include <iostream>
 #include <map>
+#include <unordered_map>
 #include <vector>
 #include <fstream>
 
 using namespace std;
 
 typedef pair<string, int> PAIR;
-typedef map<string, char> MAP_CH;
-typedef map<string, bool> MAP_BO;
-typedef map<string, string> MAP_STR;
-typedef map<string, int> MAP_INT;
-typedef map<int, PAIR> MAP_PA;
-typedef map<int, vector<string> > MAP_VEC;
+typedef unordered_map<string, char> MAP_CH;
+typedef unordered_map<string, bool> MAP_BO;
+typedef unordered_map<string, string> MAP_STR;
+typedef unordered_map<string, int> MAP_INT;
+typedef unordered_map<int, PAIR> MAP_PA;
+typedef unordered_map<int, vector<string> > MAP_VEC;
 typedef vector<PAIR> VEC_PA;
 
 int sec_num;
@@ -23,12 +24,16 @@ MAP_PA tran_sec; // sec_id, <account, money>
 MAP_VEC sec_tran; // one scetion's transactions
 MAP_INT account_balance;
 
-void bank(int now_tran[], int last_tran[], int now_sec_id, int& now_stamp, fstream& file) {
+void bank(int now_tran[], int last_tran[], int now_sec_id, int& now_stamp, 
+        fstream& file, bool finish[]) {
 
     // Do one by one
     while (1) {
 
-        if (now_tran[now_sec_id] == last_tran[now_sec_id]) return;
+        if (now_tran[now_sec_id] == last_tran[now_sec_id]) {
+            finish[now_sec_id] = true;
+            return;
+        }
         string now_tran_id = sec_tran[now_sec_id][now_tran[now_sec_id]];
         string base_id = tran_sec[now_sec_id].first;
         string forward_id = tran_forward_id[now_tran_id];
@@ -79,26 +84,36 @@ void bank(int now_tran[], int last_tran[], int now_sec_id, int& now_stamp, fstre
     }
 }
 
-bool finished(int now_tran[], int last_tran[]) {
+bool finished(int now_tran[], int last_tran[], bool finsh[]) {
     for (int now_sec_id = 0; now_sec_id < sec_num; ++now_sec_id) {
-        if (last_tran[now_sec_id] != now_tran[now_sec_id]) {
-            return false;
+        if (finsh[now_sec_id]) continue;
+        else if (last_tran[now_sec_id] == now_tran[now_sec_id]) {
+            finsh[now_sec_id] = true;
+            continue;
         }
+        else return false;
     }
     return true;
 }
 
-void schedule(int last_tran[], fstream& file) {
+void schedule(int last_tran[]) {
+
+    fstream file;
+    file.open("transaction_log.txt", fstream::out);
 
     int now_stamp = 1;
     int now_tran[sec_num];
-    for (int i = 0; i < sec_num; ++i)
+    bool finsh[sec_num];
+    for (int i = 0; i < sec_num; ++i) {
         now_tran[i] = 0;
+        finsh[i] = false;
+    }
 
 
-    while (!finished(now_tran, last_tran)) {
+    while (!finished(now_tran, last_tran, finsh)) {
         for (int now_sec_id = 0; now_sec_id < sec_num; ++now_sec_id) {
-            bank(now_tran, last_tran, now_sec_id, now_stamp, file);
+            if (finsh[now_sec_id]) continue;
+            bank(now_tran, last_tran, now_sec_id, now_stamp, file, finsh);
         }
     }
     file.close();
@@ -125,16 +140,18 @@ int main () {
         int money;
         file >> money;
 
-        tran_sec[now_sec_id] = make_pair(base_id, money);
-
         int transaction;
         file >> transaction;
         last_tran[now_sec_id] = transaction;
         vector<string> tmp_sec_tran;
 
         // 0 transaction
-        if (!transaction)
+        if (!transaction) {
             account_balance[base_id] += money;
+            continue;
+        }
+
+        tran_sec[now_sec_id] = make_pair(base_id, money);
 
         while (transaction--) {
             string tran_id;
@@ -156,9 +173,7 @@ int main () {
     file.close();
 
 
-    file.open("transaction_log.txt", fstream::out);
-
-    schedule(last_tran, file);
+    schedule(last_tran);
 
     return 0;
 }
