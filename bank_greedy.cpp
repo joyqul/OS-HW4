87a3ccf2749391aca2da19e839c22064ade0a7c0
Joyqul Chen <joy1357924680@gmail.com>
2014-01-22 12:58:57 +0800

    Greedy speed up, has bug

--- bank_greedy.cpp
+++ bank_greedy.cpp
@@ -1,20 +1,22 @@
 #include <iostream>
 #include <map>
+#include <unordered_map>
 #include <vector>
 #include <fstream>
+#define MAX 50
 
 using namespace std;
 
 typedef pair<string, int> PAIR;
-typedef map<string, char> MAP_CH;
-typedef map<string, bool> MAP_BO;
-typedef map<string, string> MAP_STR;
-typedef map<string, int> MAP_INT;
-typedef map<int, PAIR> MAP_PA;
-typedef map<int, vector<string> > MAP_VEC;
+typedef unordered_map<string, char> MAP_CH;
+typedef unordered_map<string, bool> MAP_BO;
+typedef unordered_map<string, string> MAP_STR;
+typedef unordered_map<string, int> MAP_INT;
+typedef unordered_map<int, PAIR> MAP_PA;
+typedef unordered_map<int, vector<string> > MAP_VEC;
 typedef vector<PAIR> VEC_PA;
 
-int sec_num;;
+int sec_num;
 MAP_CH tran_type; // T_id, B or T
 MAP_STR tran_forward_id; // T_id, forward_id
 MAP_INT tran_forward_money; // T_id, forward_money
@@ -23,7 +25,10 @@ MAP_INT account_balance;
 MAP_PA tran_sec; // sec_id, <account, money>
 MAP_VEC sec_tran; // one scetion's transactions
 
-void bank(int now_tran[], int last_tran[], const int now_stamp, fstream& file) {
+void bank(int now_tran[], int last_tran[], const int now_stamp, 
+        fstream& file, bool finsh[]) {
+    int now_test = 0;
+
     // Lock
     bool locked[sec_num];
     for (int i = 0; i < sec_num; ++i)
@@ -32,6 +37,8 @@ void bank(int now_tran[], int last_tran[], const int now_stamp, fstream& file) {
 
     // Find B and lock first
     for (int now_sec_id = 0; now_sec_id < sec_num; ++now_sec_id) {
+        if (now_test == MAX) return;
+        if (finsh[now_sec_id]) continue;
         if (now_tran[now_sec_id] == last_tran[now_sec_id]) continue;
         string now_tran_id = sec_tran[now_sec_id][now_tran[now_sec_id]];
         if (tran_type[now_tran_id] == 'B') {
@@ -39,23 +46,26 @@ void bank(int now_tran[], int last_tran[], const int now_stamp, fstream& file) {
             ++now_tran[now_sec_id];
             string base_id = tran_sec[now_sec_id].first;
 
-            file.open("transaction_log.txt", fstream::app);
             file << now_tran_id << " " << now_stamp << " " << account_balance[base_id] << endl;
-            cout << now_tran_id << " " << now_stamp << " " << account_balance[base_id] << endl;
+            ++now_test;
         }
     }
 
 
     // Do one by one
     for (int now_sec_id = 0; now_sec_id < sec_num; ++now_sec_id) {
+        if (now_test == MAX) return;
+        if (finsh[now_sec_id]) continue;
         if (locked[now_sec_id]) continue;
         if (now_tran[now_sec_id] == last_tran[now_sec_id]) continue;
 
+        ++now_test;
         string now_tran_id = sec_tran[now_sec_id][now_tran[now_sec_id]];
         string base_id = tran_sec[now_sec_id].first;
         string forward_id = tran_forward_id[now_tran_id];
         if (locked[tran_sec_id[forward_id]]) continue;
         
+
         int tran_money = tran_forward_money[now_tran_id];
 
         if (now_tran[now_sec_id] == 0) {
@@ -70,9 +80,7 @@ void bank(int now_tran[], int last_tran[], const int now_stamp, fstream& file) {
                 account_balance[base_id] -= tran_money;
                 account_balance[forward_id] += tran_money;
 
-                file.open("transaction_log.txt", fstream::app);
                 file << now_tran_id << " " << now_stamp << endl;
-                cout << now_tran_id << " " << now_stamp << endl;
 
                 ++now_tran[now_sec_id];
                 locked[now_sec_id] = true;
@@ -83,9 +91,7 @@ void bank(int now_tran[], int last_tran[], const int now_stamp, fstream& file) {
             account_balance[base_id] -= tran_money;
             account_balance[forward_id] += tran_money;
 
-            file.open("transaction_log.txt", fstream::app);
             file << now_tran_id << " " << now_stamp << endl;
-            cout << now_tran_id << " " << now_stamp << endl;
 
             ++now_tran[now_sec_id];
             locked[now_sec_id] = true;
@@ -95,25 +101,32 @@ void bank(int now_tran[], int last_tran[], const int now_stamp, fstream& file) {
 
 }
 
-bool finished(int last_tran[], int now_tran[]) {
+bool finished(int last_tran[], int now_tran[], bool finish[]) {
     for (int now_sec_id = 0; now_sec_id < sec_num; ++now_sec_id) {
-        if (last_tran[now_sec_id] != now_tran[now_sec_id]) {
-            return false;
+        if (finish[now_sec_id]) continue;
+        else if (last_tran[now_sec_id] == now_tran[now_sec_id]) {
+            finish[now_sec_id] = true;
         }
+        else return false;
     }
     return true;
 }
 
-void schedule(int last_tran[], fstream& file) {
+void schedule(int last_tran[]) {
 
     int now_stamp = 1;
     int now_tran[sec_num];
-    for (int i = 0; i < sec_num; ++i)
+    bool finish[sec_num];
+    for (int i = 0; i < sec_num; ++i) {
         now_tran[i] = 0;
+        finish[i] = false;
+    }
 
+    fstream file;
+    file.open("transaction_log.txt", fstream::out);
 
-    while (!finished(last_tran, now_tran)) {
-        bank(now_tran, last_tran, now_stamp, file);
+    while (!finished(last_tran, now_tran, finish)) {
+        bank(now_tran, last_tran, now_stamp, file, finish);
         ++now_stamp;
     }
 
@@ -123,7 +136,8 @@ void schedule(int last_tran[], fstream& file) {
 int main () {
 
     // read the transaction.txt
-    fstream file("transactions.txt", fstream::in);
+    fstream file;
+    file.open("transactions.txt", fstream::in);
 
     file >> sec_num;
 
@@ -135,10 +149,16 @@ int main () {
         int money;
         file >> money;
 
-        tran_sec[now_sec_id] = make_pair(base_id, money);
 
         int transaction;
         file >> transaction;
+
+        if (!transaction) {
+            account_balance[base_id] = money;
+            continue;
+        }
+        
+        tran_sec[now_sec_id] = make_pair(base_id, money);
         last_tran[now_sec_id] = transaction;
         vector<string> tmp_sec_tran;
 
@@ -162,9 +182,8 @@ int main () {
     file.close();
 
     
-    file.open("transaction_log.txt", fstream::out);
 
-    schedule(last_tran, file);
+    schedule(last_tran);
 
     return 0;
 }
